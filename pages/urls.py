cb145db73e31cbb80448f89f5887431541f3a88c
miveh <imivechi@gmail.com>
2021-08-04 22:40:05 +0430

    chaptur4,pages app

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 # pages/urls.py
 from django.urls import path
-from .views import home_page_view
+# from .views import home_page_view
+from .views import HomePageView
 
 urlpatterns = [
-    path('', home_page_view, name='home')
+    # path('', home_page_view, name='home')
+    path('', HomePageView.as_view(), name='home'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse('Hello, World!')
+# def home_page_view(request):
+#     return HttpResponse('Hello, World!')
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
